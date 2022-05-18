c109af569666fc5d16b34faa7d9e828578db5a5d
roomanasa <roomana.sadiq@gmail.com>
2022-05-18 16:42:02 -0400

    updated person table

--- sql/V2__Add_person.sql
+++ sql/V2__Add_person.sql
@@ -1,3 +1,6 @@
 insert into person (id, name) values (1, 'Axel');
 insert into person (id, name) values (2, 'Mr. Foo');
 insert into person (id, name) values (3, 'Ms. Bar');
+update person set name='Roomana' where id =1;
+update person set name='Samir' where id =2;
+update person set name='Farha' where id =3;
